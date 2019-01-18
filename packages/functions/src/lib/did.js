734b60f0b24678c936a04f006b8c472ac13ae7b8
Orie Steele <orie@ohs.io>
2019-01-18 17:16:17 -0600

    fix tests

--- packages/functions/src/lib/did.js
+++ packages/functions/src/lib/did.js
@@ -1,4 +1,4 @@
-const ghdid = require('@transmute/github-did');
+const ghdid = require('@github-did/lib');
 
 const aliceDID = 'did:ghdid:transmute-industries~github-did~1bed11140547b8407478bdf2650db50a5a0c18ef2ae4caf20e818a9433923c2a';
 const bobDID = 'did:ghdid:transmute-industries~github-did~f3b3f869f844ff1bc18b59b41d9064a792cc699d5512d8f98d75d5ece623b28c';
