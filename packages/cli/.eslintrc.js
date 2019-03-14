e377440c8709d7fbe4021ebf566f3a88bac5f71f
gjgd <gjgd@protonmail.com>
2019-03-14 18:14:40 -0500

    Add eslint only in cli package

---
+++ packages/cli/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:jest/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+  },
+};
