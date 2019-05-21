9747b07e01697d8e6eb6df1e857da6c90982ca06
Bongani Mbigi <bongani@2014-Forest-Hill-Drive.localdomain>
2019-05-21 12:56:57 -0500

    Added Google Analytics

--- packages/app/src/index.js
+++ packages/app/src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import withTracker from './utils/analytics';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const TrackedApp = withTracker(App);
+
+ReactDOM.render(<TrackedApp />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
